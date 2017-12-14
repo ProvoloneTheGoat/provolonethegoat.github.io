29bfcdc0716b8f1f3333638bd4f4d843c0c06077
ProvoloneTheGoat <31249394+ProvoloneTheGoat@users.noreply.github.com>
2017-12-14 08:37:40 -0700

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,20 +1,72 @@
-$( function() {
-    $( "#dialog" ).dialog();
-  } );
-  
-  
-$( ".FrenchButton" ).dialog({
-  buttons: [
-    {
-      text: "Ok",
-      icon: "ui-icon-heart",
-      click: function() {
-        $( this ).dialog( "close" );
-      }
- 
-      // Uncommenting the following line would hide the text,
-      // resulting in the label being used as a tooltip
-      //showText: false
-    }
-  ]
+$(document).ready(function(){
+  $("#PPS").click(function () {
+    $( "#PPS" ).dialog();
+  });
 });
+
+$(document).ready(function(){
+  $("#NPL").click(function () {
+    $( "#NPL" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#SDP").click(function () {
+    $( "#SDP" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#TNY").click(function () {
+    $( "#TNY" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#PMS").click(function () {
+    $( "#PMS" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#MSS").click(function () {
+    $( "#MSS" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#MDN").click(function () {
+    $( "#MDN" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#LCA").click(function () {
+    $( "#LCA" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#SWZ").click(function () {
+    $( "#SWZ" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#AUT").click(function () {
+    $( "#AUT" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#FRA").click(function () {
+    $( "#FRA" ).dialog();
+  });
+});
+
+$(document).ready(function(){
+  $("#OTM").click(function () {
+    $( "#OTM" ).dialog();
+  });
+});
+
